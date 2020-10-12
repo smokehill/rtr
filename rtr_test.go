73ac9ff83d1ed249c3eaf178ecc81270f161909a
smokehill <valera.padolochniy@gmail.com>
2020-10-12 13:35:44 +0300

    testing prepareRouteMethods

--- rtr_test.go
+++ rtr_test.go
@@ -12,7 +12,7 @@ func TestRoutes(t *testing.T) {
 	r.SetRoute("GET", "/news/([0-9]+)", func (w http.ResponseWriter, r *http.Request) {})
 
 	if len(r.routes) != 2 {
-		t.Errorf("r.routes result must be equal to 2, but got %d", len(r.routes))
+		t.Errorf("len(r.routes) must be equal to 2, but got %d", len(r.routes))
 	}
 }
 
@@ -21,6 +21,25 @@ func TestSplitURL(t *testing.T) {
 	r := SplitURL(u)
 
 	if len(r) != 2 {
-		t.Errorf("SplitURL(u) result must be equal to 2, buy got %d", len(r))
+		t.Errorf("len(r) must be equal to 2, but got %d", len(r))
+	}
+}
+
+func TestPrepareRouteMethods(t *testing.T) {
+	methods := "GET,POST,PUT,DELETE"
+	r := prepareRouteMethods(methods)
+
+	if len(r) != 4 {
+		t.Errorf("len(r) must be equal to 4, but got %d", len(r))
+	} else {
+		if r[0] != "GET" {
+			t.Errorf("r[0] must be equal to \"GET\", but got %s", r[0])
+		} else if r[1] != "POST" {
+			t.Errorf("r[1] must be equal to \"POST\", but got %s", r[1])
+		} else if r[2] != "PUT" {
+			t.Errorf("r[2] must be equal to \"PUT\", but got %s", r[2])
+		} else if r[3] != "DELETE" {
+			t.Errorf("r[3] must be equal to \"DELETE\", but got %s", r[3])
+		}
 	}
 }
